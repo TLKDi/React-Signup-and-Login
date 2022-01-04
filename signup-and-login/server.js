fd26e42580fa73ce90c60261336e1f9d6d3a5787
Tarik El Kadi <elkadi.tarik@googlemail.com>
2022-01-05 00:47:32 +0100

    db connection, data schema and model

--- signup-and-login/server.js
+++ signup-and-login/server.js
@@ -4,7 +4,24 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const port = 3001;
 
+//db connection
+mongoose.connect('mongodb+srv://tarik-admin:aEdumaztV1CazT1O@cluster0.qqzjw.mongodb.net/usersDB?retryWrites=true&w=majority');
+
+//data schema
+const userSchema  = {
+    vorname : String, 
+    nachname: String, 
+    alter: Number, 
+    email: String, 
+    passwort: String
+} 
+
+//Routes
 
 
+//data model
+const User = mongoose.model("User", userSchema);
 
-
+app.listen(port, function(){
+    console.log("Server is running");
+});
