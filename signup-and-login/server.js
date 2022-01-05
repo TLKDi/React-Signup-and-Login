1adbfbb8f4130f96235a09d116091da655ddd0ec
Tarik El Kadi <elkadi.tarik@googlemail.com>
2022-01-05 01:57:34 +0100

    completed addUser method, create route

--- signup-and-login/server.js
+++ signup-and-login/server.js
@@ -4,6 +4,11 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const port = 3001;
 
+//config
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
 //db connection
 mongoose.connect('mongodb+srv://tarik-admin:aEdumaztV1CazT1O@cluster0.qqzjw.mongodb.net/usersDB?retryWrites=true&w=majority');
 
@@ -24,6 +29,20 @@ app.get('/users', (req, res) => {
     User.find()
     .then(items => res.json(items))
     .catch(err => res.status(400).json("Error: " + err));
+});
+
+app.post('/newUser', (req,res) => {
+    const newUser = new User({
+        vorname : res.body.vorname, 
+        nachname: res.body.nachname, 
+        alter: res.body.alter, 
+        email: res.body.email, 
+        passwort: res.body.passwort
+    });
+    newUser.save()
+    .then((item) => console.log(item))
+    .catch(err => res.status(400).json("Error: " + err));
+
 });
 
 app.listen(port, function(){
